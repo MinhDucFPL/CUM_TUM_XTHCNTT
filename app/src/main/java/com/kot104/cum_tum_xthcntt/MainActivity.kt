ea926355748ebcde2f0dfbafe113a497e4f89183
Minh Duc <duclm.fpl.work@gmail.com>
2024-05-31 22:40:02 +0700

    Start Project

---
+++ app/src/main/java/com/kot104/cum_tum_xthcntt/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kot104.cum_tum_xthcntt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kot104.cum_tum_xthcntt.ui.theme.CUM_TUM_XTHCNTTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CUM_TUM_XTHCNTTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CUM_TUM_XTHCNTTTheme {
+        Greeting("Android")
+    }
+}
